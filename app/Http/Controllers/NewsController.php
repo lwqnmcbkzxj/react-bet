9364ba17ccec03648be9f6412c83759731db3061
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2020-05-04 14:22:16 +0300

    Added News queries

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GetNewsRequest;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function get(GetNewsRequest $request)
+    {
+
+    }
+    public function detail(GetNewsRequest $request)
+    {
+
+    }
+
+}
