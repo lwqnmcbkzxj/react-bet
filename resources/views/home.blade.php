9cebb72b7313d5039bc1929d4b56fbc0dcb73207
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2020-05-12 05:39:29 +0300

    Few changes of API

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
+                <meta name="yandex-verification" content="177902f2731cfaeb" />
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <meta name="yandex-verification" content="177902f2731cfaeb" />
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
