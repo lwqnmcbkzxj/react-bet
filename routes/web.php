1dff8904671f45f19b210bd4ead6fc49f59ebbf3
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2020-05-20 10:41:21 +0300

    Route for files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Http\File;
 use Illuminate\Support\Facades\Route;
+Route::get('storage/{filename}', function ($filename)
+{
+    $path = storage_path('app\public\\' . $filename);
 
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
