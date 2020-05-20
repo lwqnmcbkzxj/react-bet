ecc6fd524e62e744276f40b0b92716d3f6e3a38d
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2020-05-20 11:02:29 +0300

    Changes sport model

--- app/Sport.php
+++ app/Sport.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Sport extends Model
 {
     protected $guarded = ['id'];
+    protected $hidden = ['logo'];
+    protected $appends = ['image'];
+    function getImageAttribute()
+    {
+        return Storage::url('bookmakers/'. $this->logo);
+    }
 }
