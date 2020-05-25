e3a6b49fea7ad00d77125c781cf332814ce92972
Maxim Bezdenezhnykh <makss567@yandex.ru>
2020-05-25 18:59:32 +0500

    ihttpclient file fix

---
+++ iOS/BettingHub/BettingHub/Source/Services/HttpClient/IHttpClient.swift
@@ -0,0 +1,19 @@
+//
+//  I HttpClient.swift
+//  CryptoTest
+//
+//  Created by Maxim Bezdenezhnykh on 17.02.2020.
+//  Copyright © 2020 Maxim Bezdenezhnykh. All rights reserved.
+//
+
+import Foundation
+
+protocol IHttpClient: class {
+    func request(request: URLRequest,
+                 callback: ((Result<Data, Error>)->Void)?)
+    
+    //TODO: Delete when response codes added to server
+    func authRequest(request: URLRequest,
+                     callback: ((BHError?) -> Void)?)
+}
+
