7c36ab2da1e0c3e2dabbbe199b52dd81dde2a0bb
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2020-06-10 02:37:00 +0300

    Dashboard fixed

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,15 +14,16 @@ use App\Exports\UsersExport;
 class DashboardController extends Controller
 {
     public function index() {
-        $date = new \DateTime(null, new \DateTimeZone('Europe/Moscow'));
 
         return $this->sendResponse([
             'users_count' => User::where('role_id', 1)->count(),
             'forecasts_count' => Forecast::count(),
             'posts_count' => Post::count(),
             'news_count' => News::count(),
-            'users_count_today' => User::where('role_id', 1)->where('created_at', '>=', $date->format('Y-m-d'))->count(),
-            'forecasts_count_today' => Forecast::where('created_at', '>=', $date->format('Y-m-d'))->count(),
+            'users_count_today' => User::where('role_id', 1)->where('created_at', '>=', now()->format('Y-m-d'))->count(),
+            'forecasts_count_today' => Forecast::where('created_at', '>=', now()->format('Y-m-d'))->count(),
+            'posts_count_today' => Forecast::where('created_at', '>=', now()->format('Y-m-d'))->count(),
+            'news_count_today' => Forecast::where('created_at', '>=', now()->format('Y-m-d'))->count(),
         ],'Success',200);
     }
 
