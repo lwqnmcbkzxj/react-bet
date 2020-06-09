4698e22a8cf65c9d88fd338612513d66cff05573
Pavel Lapotnikov <38629071+lpvmak@users.noreply.github.com>
2020-06-09 21:55:29 +0300

    Event ranking

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventCollection;
+use Illuminate\Support\Facades\DB;
 use function GuzzleHttp\describe_type;
 
 class EventController extends Controller
@@ -17,14 +18,20 @@ class EventController extends Controller
     public function getAll(Request $request)
     {
         $date = new DateTime('now', new \DateTimeZone('Europe/Moscow'));
-        $events = Event::query();
+        $events = Event::query()->leftJoin(DB::raw(" (SELECT
+        `event_id`, COUNT(`event_id`) as forecasts_count
+    FROM
+        api_betting_hub.forecasts
+    GROUP BY `event_id`) as T"),'event_id','=','id');;
         if (!$request->has('limit') || $request['limit'] == 0) {
-            $request['limit'] = 6;
+            $request['limit'] = 15;
         }
         if ($request->has('order_by')) {
-            $events = $events->orderBy($request['order_by'], $request['direction']?$request['direction']:'DESC');
+            $events = $events->orderBy($request['order_by'], $request['direction'] ? $request['direction'] : 'DESC');
         }
-        $events = $events->orderBy('id', 'desc')->paginate($request['limit']);
+        else
+            $events = $events->orderBy('forecasts_count','desc');
+        $events = $events->paginate($request['limit']);
         $events = new EventCollection($events);
         return $this->sendResponse(($events), 'Success', 200);
     }
